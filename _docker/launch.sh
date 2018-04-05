a882bcd2f212b34be4aa0cc0671ab2f872d97af5
Jakub Luczynski <jakub.luczynski@gmail.com>
2018-04-05 20:14:09 +0200

    Fixes

--- _docker/launch.sh
+++ _docker/launch.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker-compose -f "${DIR}/docker-compose.yml" down -v
-docker-compose -f "${DIR}/docker-compose.yml" up -d
+docker-compose -f "${DIR}/docker-compose.yml" up
